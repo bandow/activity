8eb9ca2baa270e0bb47aa97ba7bfb1074dce9c81
goSnowbank <sycbbb@sina.com>
2017-04-24 16:47:55 +0800

    activity_area

---
+++ activity_area/scripts/activityArea.js
@@ -0,0 +1,32 @@
+var app = new Vue({
+	el:"#app",
+	data:{
+		title:'活动专区',
+		list:new Array()
+	},
+	mounted:function(){
+		var _this = this;
+		this.$nextTick(function(){
+			axios.get('http://10.44.172.245/activity/activity_area/data/activity.json')
+			.then(
+				function(retObj){
+					if(retObj.status == 200){
+						app.list = retObj.data.result.list;
+					}
+				}
+			)
+			.catch(function(errorObj){
+				console.log("get data error...");
+			})
+		});
+	},
+	methods:{
+	},
+	watch:{
+	},
+	computed:{	
+	},
+	filters:{
+	}
+});
+
